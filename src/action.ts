79795423171c4d8e20c1922b9ec3ada68d32439b
hanseltime <hanseltime@gmail.com>
2024-03-04 00:05:35 -0700

    feat: adding updateAfterRefBehavior

--- src/action.ts
+++ src/action.ts
@@ -11,6 +11,7 @@ const options: GithubOptions = {
   titleMsg: core.getInput("titleMsg") || undefined,
   prToBranch: core.getInput("prToBranch") || undefined,
   remoteRepoToken: core.getInput("remoteRepoToken") || undefined,
+  updateAfterRef: core.getInput("updateAfterRef").toLowerCase() === "true",
 };
 
 void syncGithubRepo(options);
