04f178b40cb7c7508155fe77f45947ad5273b0f5
hanseltime <hanseltime@gmail.com>
2026-02-08 13:07:18 -0700

    fix: sync github actions template

--- release.config.js
+++ release.config.js
@@ -1,42 +1,48 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const { readFileSync } = require("fs");
 const { join } = require("path");
 
 const packageJson = JSON.parse(
-  readFileSync(join(__dirname, "package.json")).toString(),
+	readFileSync(join(__dirname, "package.json")).toString(),
 );
 
 // Abbreviate for Git Commit readability
 const fullName = packageJson.name;
 const scopeLimiterIdx = fullName.lastIndexOf("/");
 const abbreviatedName = fullName.substring(
-  scopeLimiterIdx >= 0 ? scopeLimiterIdx + 1 : 0,
+	scopeLimiterIdx >= 0 ? scopeLimiterIdx + 1 : 0,
 );
 
 module.exports = {
-  branches: ["main", { name: "alpha", prerelease: true }],
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    "@semantic-release/npm",
-    [
-      "@semantic-release/exec",
-      {
-        prepareCmd: "npm run bundle",
-      },
-    ],
-    [
-      "@semantic-release/git",
-      {
-        assets: ["CHANGELOG.md", "package.json", "bundle/**"],
-        message: `docs(release): ${abbreviatedName} $\{nextRelease.version} [skip ci]\n\n$\{nextRelease.notes}`,
-      },
-    ],
-    // This increments our major release tag to the current sha
-    "semantic-release-major-tag",
-    // This creates a release on github - you can decide if you want to mirror the files in package.json
-    "@semantic-release/github",
-  ],
-  ci: true,
+	branches: ["main", { name: "alpha", prerelease: true }],
+	plugins: [
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/release-notes-generator",
+		"@semantic-release/changelog",
+		"@semantic-release/npm",
+		[
+			"@semantic-release/exec",
+			{
+				prepareCmd: "npm run bundle",
+			},
+		],
+		// format any changed files
+		[
+			"@semantic-release/exec",
+			{
+				prepareCmd: "npm run format -- --fix",
+			},
+		],
+		[
+			"@semantic-release/git",
+			{
+				assets: ["CHANGELOG.md", "package.json", "bundle/**"],
+				message: `docs(release): ${abbreviatedName} $\{nextRelease.version} [skip ci]\n\n$\{nextRelease.notes}`,
+			},
+		],
+		// This increments our major release tag to the current sha
+		"semantic-release-major-tag",
+		// This creates a release on github - you can decide if you want to mirror the files in package.json
+		"@semantic-release/github",
+	],
+	ci: true,
 };
--- src/sync-github-repo.ts
+++ src/sync-github-repo.ts
@@ -5,189 +5,189 @@ import { tmpdir } from "os";
 import { mkdtemp } from "fs/promises";
 import { execSync } from "child_process";
 import {
-  syncResultsToMd,
-  TEMPLATE_SYNC_LOCAL_CONFIG,
-  templateSync,
+	syncResultsToMd,
+	TEMPLATE_SYNC_LOCAL_CONFIG,
+	templateSync,
 } from "@hanseltime/template-repo-sync";
 import {
-  DEFAULT_BRANCH_PREFIX,
-  DEFAULT_COMMIT_MSG,
-  DEFAULT_TITLE_MSG,
+	DEFAULT_BRANCH_PREFIX,
+	DEFAULT_COMMIT_MSG,
+	DEFAULT_TITLE_MSG,
 } from "./constants";
 import { getBranchName } from "./get-branch-name";
 import { withoutGhExtraHeader } from "./git-utils";
 import { writeFileSync } from "fs";
 
 export interface GithubOptions {
-  /**
-   * The owner/repo path of the repo on github
-   */
-  repoPath: string;
+	/**
+	 * The owner/repo path of the repo on github
+	 */
+	repoPath: string;
 
-  /** A github token with access to write pull requests */
-  githubToken: string;
+	/** A github token with access to write pull requests */
+	githubToken: string;
 
-  /** A separate github token with permissions to clone from the other repo.  Only needed for private repos */
-  remoteRepoToken?: string;
+	/** A separate github token with permissions to clone from the other repo.  Only needed for private repos */
+	remoteRepoToken?: string;
 
-  /**
-   * Normally, this should be the working directory of a github action, but this
-   * could also be an internal file depending on things like monorepo structures
-   */
-  repoRoot?: string;
+	/**
+	 * Normally, this should be the working directory of a github action, but this
+	 * could also be an internal file depending on things like monorepo structures
+	 */
+	repoRoot?: string;
 
-  /**
-   * The branch on the template that we want to sync to
-   */
-  templateBranch: string;
+	/**
+	 * The branch on the template that we want to sync to
+	 */
+	templateBranch: string;
 
-  /** A short branch prefix for the branch that will be created in order to store the changes */
-  branchPrefix?: string;
+	/** A short branch prefix for the branch that will be created in order to store the changes */
+	branchPrefix?: string;
 
-  /**
-   * The commit message to supply when making the merge commits
-   */
-  commitMsg?: string;
+	/**
+	 * The commit message to supply when making the merge commits
+	 */
+	commitMsg?: string;
 
-  /**
-   * The title of the pull request
-   */
-  titleMsg?: string;
+	/**
+	 * The title of the pull request
+	 */
+	titleMsg?: string;
 
-  /**
-   * This is the branch to open the pull request to.  If not set, we will use the
-   * repo's default branch.
-   */
-  prToBranch?: string;
+	/**
+	 * This is the branch to open the pull request to.  If not set, we will use the
+	 * repo's default branch.
+	 */
+	prToBranch?: string;
 
-  /**
-   * Updates the afterRef in a templatesync.local.json to be the last sha value of the
-   * template if true
-   */
-  updateAfterRef: boolean;
+	/**
+	 * Updates the afterRef in a templatesync.local.json to be the last sha value of the
+	 * template if true
+	 */
+	updateAfterRef: boolean;
 
-  /**
-   * For testing, this is a templatesync.config.json for mocking
-   */
-  mockLocalConfig?: string;
+	/**
+	 * For testing, this is a templatesync.config.json for mocking
+	 */
+	mockLocalConfig?: string;
 }
 
 function getTempDir() {
-  return process.env["RUNNER_TEMP"] || tmpdir();
+	return process.env["RUNNER_TEMP"] || tmpdir();
 }
 
 export async function syncGithubRepo(options: GithubOptions) {
-  const octokit = github.getOctokit(options.githubToken);
+	const octokit = github.getOctokit(options.githubToken);
 
-  const repoRoot = options.repoRoot ?? process.cwd();
-  const branchPrefix = options.branchPrefix ?? DEFAULT_BRANCH_PREFIX;
-  const commitMsg = options.commitMsg ? options.commitMsg : DEFAULT_COMMIT_MSG;
+	const repoRoot = options.repoRoot ?? process.cwd();
+	const branchPrefix = options.branchPrefix ?? DEFAULT_BRANCH_PREFIX;
+	const commitMsg = options.commitMsg ? options.commitMsg : DEFAULT_COMMIT_MSG;
 
-  // Note, we use git here so that we can change this around for other git providers more easily
-  const baseRepoUrl = `github.com/${options.repoPath}.git`;
-  const authedRepoUrl = `https://${options.remoteRepoToken ? `github_actions:${options.remoteRepoToken}@` : ""}${baseRepoUrl}`;
+	// Note, we use git here so that we can change this around for other git providers more easily
+	const baseRepoUrl = `github.com/${options.repoPath}.git`;
+	const authedRepoUrl = `https://${options.remoteRepoToken ? `github_actions:${options.remoteRepoToken}@` : ""}${baseRepoUrl}`;
 
-  const templateBranchOpts = {
-    branchPrefix,
-    templateBranch: options.templateBranch,
-    repoUrl: authedRepoUrl,
-    repoRoot,
-  };
+	const templateBranchOpts = {
+		branchPrefix,
+		templateBranch: options.templateBranch,
+		repoUrl: authedRepoUrl,
+		repoRoot,
+	};
 
-  const branchName = options.remoteRepoToken
-    ? withoutGhExtraHeader(() => getBranchName(templateBranchOpts))
-    : getBranchName(templateBranchOpts);
+	const branchName = options.remoteRepoToken
+		? withoutGhExtraHeader(() => getBranchName(templateBranchOpts))
+		: getBranchName(templateBranchOpts);
 
-  // Check if the branch exists already and skip
-  const output = execSync(`git ls-remote --heads origin "${branchName}"`)
-    .toString()
-    .trim();
-  // Non-empty output means the branch exists
-  if (output) {
-    core.warning(
-      `The exact same combination of ${TEMPLATE_SYNC_LOCAL_CONFIG} and remote ${options.templateBranch} has been run before`,
-    );
-    core.warning(
-      `If you would like to re-run due to plugins, etc.  Please delete branch: ${branchName}`,
-    );
-    // Error this
-    process.exit(1);
-  }
+	// Check if the branch exists already and skip
+	const output = execSync(`git ls-remote --heads origin "${branchName}"`)
+		.toString()
+		.trim();
+	// Non-empty output means the branch exists
+	if (output) {
+		core.warning(
+			`The exact same combination of ${TEMPLATE_SYNC_LOCAL_CONFIG} and remote ${options.templateBranch} has been run before`,
+		);
+		core.warning(
+			`If you would like to re-run due to plugins, etc.  Please delete branch: ${branchName}`,
+		);
+		// Error this
+		process.exit(1);
+	}
 
-  let prToBranch = options.prToBranch;
-  if (!prToBranch) {
-    const resp = await octokit.rest.repos.get({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-    });
-    prToBranch = resp.data.default_branch;
-  }
+	let prToBranch = options.prToBranch;
+	if (!prToBranch) {
+		const resp = await octokit.rest.repos.get({
+			owner: github.context.repo.owner,
+			repo: github.context.repo.repo,
+		});
+		prToBranch = resp.data.default_branch;
+	}
 
-  const origRef = execSync(
-    "git symbolic-ref --short -q HEAD ||  git rev-parse HEAD",
-  ).toString();
+	const origRef = execSync(
+		"git symbolic-ref --short -q HEAD ||  git rev-parse HEAD",
+	).toString();
 
-  try {
-    // Checkout the branch from the "to branch"
-    execSync(`git fetch origin ${prToBranch}`);
-    execSync(`git checkout ${prToBranch}`);
-    // stash everything except "added" files since we will assume this means there's an intention
-    execSync(`git stash --include-untracked`);
-    console.log(`Checking out ${branchName}`);
-    execSync(`git checkout -b ${branchName}`);
-    if (options.mockLocalConfig) {
-      writeFileSync(
-        join(repoRoot, `${TEMPLATE_SYNC_LOCAL_CONFIG}.json`),
-        options.mockLocalConfig,
-      );
-    }
+	try {
+		// Checkout the branch from the "to branch"
+		execSync(`git fetch origin ${prToBranch}`);
+		execSync(`git checkout ${prToBranch}`);
+		// stash everything except "added" files since we will assume this means there's an intention
+		execSync(`git stash --include-untracked`);
+		console.log(`Checking out ${branchName}`);
+		execSync(`git checkout -b ${branchName}`);
+		if (options.mockLocalConfig) {
+			writeFileSync(
+				join(repoRoot, `${TEMPLATE_SYNC_LOCAL_CONFIG}.json`),
+				options.mockLocalConfig,
+			);
+		}
 
-    // Clone and merge on this branch
-    const tempAppDir = await mkdtemp(join(getTempDir(), "template_sync_"));
+		// Clone and merge on this branch
+		const tempAppDir = await mkdtemp(join(getTempDir(), "template_sync_"));
 
-    console.log("Calling template sync...");
-    const templateSyncOptions = {
-      tmpCloneDir: tempAppDir,
-      repoDir: options.repoRoot ?? process.cwd(),
-      repoUrl: authedRepoUrl,
-      updateAfterRef: options.updateAfterRef,
-      branch: options.templateBranch,
-    };
-    const result = options.remoteRepoToken
-      ? await withoutGhExtraHeader(async () =>
-          templateSync(templateSyncOptions),
-        )
-      : await templateSync(templateSyncOptions);
+		console.log("Calling template sync...");
+		const templateSyncOptions = {
+			tmpCloneDir: tempAppDir,
+			repoDir: options.repoRoot ?? process.cwd(),
+			repoUrl: authedRepoUrl,
+			updateAfterRef: options.updateAfterRef,
+			branch: options.templateBranch,
+		};
+		const result = options.remoteRepoToken
+			? await withoutGhExtraHeader(async () =>
+					templateSync(templateSyncOptions),
+				)
+			: await templateSync(templateSyncOptions);
 
-    console.log("Committing all files...");
-    // commit everything
-    execSync("git add .");
-    execSync(`git commit -m "${commitMsg}"`);
-    execSync(`git push --set-upstream origin "${branchName}"`);
+		console.log("Committing all files...");
+		// commit everything
+		execSync("git add .");
+		execSync(`git commit -m "${commitMsg}"`);
+		execSync(`git push --set-upstream origin "${branchName}"`);
 
-    console.log("Creating Pull Request...");
-    const resp = await octokit.rest.pulls.create({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      head: branchName,
-      base: prToBranch,
-      title: DEFAULT_TITLE_MSG,
-      body: `
+		console.log("Creating Pull Request...");
+		const resp = await octokit.rest.pulls.create({
+			owner: github.context.repo.owner,
+			repo: github.context.repo.repo,
+			head: branchName,
+			base: prToBranch,
+			title: DEFAULT_TITLE_MSG,
+			body: `
     Template Synchronization Operation of ${baseRepoUrl} ${options.templateBranch}
 
     ${syncResultsToMd(result)}
     `,
-    });
+		});
 
-    core.setOutput("prNumber", resp.data.number);
-  } finally {
-    console.log(`Resetting to orignal ref: ${origRef}`);
-    execSync("git reset --hard");
-    execSync(`git checkout ${origRef}`);
-    // In case there were no changes
-    const stashStr = execSync(`git stash list`).toString();
-    if (stashStr.includes("stash@")) {
-      execSync(`git stash pop`);
-    }
-  }
+		core.setOutput("prNumber", resp.data.number);
+	} finally {
+		console.log(`Resetting to orignal ref: ${origRef}`);
+		execSync("git reset --hard");
+		execSync(`git checkout ${origRef}`);
+		// In case there were no changes
+		const stashStr = execSync(`git stash list`).toString();
+		if (stashStr.includes("stash@")) {
+			execSync(`git stash pop`);
+		}
+	}
 }
